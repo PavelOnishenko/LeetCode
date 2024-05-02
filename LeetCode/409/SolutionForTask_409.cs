183622312c027461319109658ca78550f86bd5d3
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-02 10:44:57 +0200

    Task 409 - Longest Palindrome.

---
+++ LeetCode/409/SolutionForTask_409.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Task_409;
+
+public class Solution
+{
+    public int LongestPalindrome(string s)
+    {
+        var charsAndCounts = s.Distinct()
+            .Select(uniqueChar =>
+                new {
+                    uniqueChar,
+                    count = s.Count(stringCharacter =>
+                        stringCharacter == uniqueChar)
+                }).ToArray();
+        var pairsOnly = charsAndCounts.Select(x =>
+            new {
+                x.uniqueChar,
+                count = x.count % 2 == 0 ? x.count : x.count - 1
+            });
+        var result = pairsOnly.Sum(x => x.count);
+        var anyHasOddCount = charsAndCounts.Any(x => x.count % 2 != 0);
+        if (anyHasOddCount)
+            result++;
+        return result;
+    }
+}
