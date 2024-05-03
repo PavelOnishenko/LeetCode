981813cfba71ecaf934bcf9711c5a80c6edef927
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-03 12:55:50 +0200

    367. Valid Perfect Square.

---
+++ LeetCode/367/SolutionForTask_367.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Task_367;
+
+public class Solution
+{
+    public bool IsPerfectSquare(int num)
+    {
+        var sqrt = Math.Sqrt(num);
+        return Math.Abs(sqrt - (int)Math.Round(sqrt)) < 0.00001;
+    }
+}
