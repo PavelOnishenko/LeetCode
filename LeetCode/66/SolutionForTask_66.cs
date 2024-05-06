e250df3f1913d2f458efe9b42712bd4ac38e7d9b
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-06 10:43:00 +0200

    Task 120.

---
+++ LeetCode/66/SolutionForTask_66.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode.Task_66;
+
+internal class SolutionForTask_66
+{
+    public int[] PlusOne(int[] digits)
+    {
+        var resultList = new List<int>(digits);
+        var lastIndex = resultList.Count - 1;
+        resultList[lastIndex]++;
+        var i = lastIndex;
+        while (resultList[i] == 10)
+        {
+            resultList[i] = 0;
+            if (i == 0)
+            {
+                resultList = new[] { 0 }.Concat(resultList).ToList();
+                i++;
+            }
+            resultList[i - 1]++;
+            i--;
+        }
+        return resultList.ToArray();
+    }
+}
