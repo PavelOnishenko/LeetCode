e1957132ce992544683899ed245f173b0cca6a08
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-06 10:43:07 +0200

    Task 120.

---
+++ LeetCode/120/SolutionForTask_120.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._120;
+
+internal class SolutionForTask_120
+{
+    public int MinimumTotal(IList<IList<int>> triangle)
+    {
+        var lastRowIndex = triangle.Count - 1;
+        for (var row = lastRowIndex - 1; row >= 0; row--)
+        {
+            for (var indexInRow = 0; indexInRow < triangle[row].Count; indexInRow++)
+            {
+                triangle[row][indexInRow] += Math.Min(triangle[row + 1][indexInRow], triangle[row + 1][indexInRow + 1]);
+            }
+        }
+        return triangle[0][0];
+    }
+}
