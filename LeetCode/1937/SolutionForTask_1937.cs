f99e266db2f0735b4f23b0bc6a99fb7f3fe572ef
PavelOnishenko <120336683+PavelOnishenko@users.noreply.github.com>
2024-05-10 20:38:49 +0200

    Problem 1937 - straightforward implementation. Fails time limit on 8x10 matrix.

---
+++ LeetCode/1937/SolutionForTask_1937.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode.Problem_1937;
+
+internal class SolutionForTask_1937
+{
+    public long MaxPoints(int[][] points)
+    {
+        var rowLength = points[0].Length;
+        var rowCount = points.Length;
+        var optionsCount = Math.Pow(rowLength, rowCount);
+        var result = long.MinValue;
+        for (var optionCode = 0; optionCode < optionsCount; optionCode++)
+        {
+            var optionCounter = optionCode;
+            var pointsCount = 0;
+            int? previousColumn = null;
+            for (var row = 0; row < rowCount; row++)
+            {
+                var column = optionCounter % rowLength;
+                var currentNumber = points[row][column];
+                var delta = previousColumn.HasValue ? Math.Abs(column - previousColumn.Value) : 0;
+                pointsCount += currentNumber - delta;
+                optionCounter /= rowLength;
+                previousColumn = column;
+            }
+            if (pointsCount > result)
+            {
+                result = pointsCount;
+            }
+        }
+        return result;
+    }
+}
